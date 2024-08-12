7fc7f6a9886e72d56ac889e6ce89800eaa51bc41
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-12 15:22:02 +0330

    feat(imagick): return error from functions

--- processes/resize.go
+++ processes/resize.go
@@ -2,49 +2,52 @@ package processes
 
 import (
 	"fmt"
-	"log"
 	"time"
 
 	"github.com/h2non/bimg"
 )
 
-func resize(path string, width, height int) {
+func Resize(path string, width, height int) error {
 	buffer, err := bimg.Read(path)
 	if err != nil {
-		log.Fatalln("unable to find the path specified")
+		return fmt.Errorf("unable to find the path specified\n")
 	}
 
 	newImage, err := bimg.NewImage(buffer).Resize(width, height)
 	if err != nil {
-		log.Fatalln("unable to resize image")
+		return fmt.Errorf("unable to resize image\n")
 	}
 
 	size, err := bimg.NewImage(newImage).Size()
 	if size.Width != width || size.Height != height {
-		log.Fatalln("unable to resize and scale image properly")
+		return fmt.Errorf("unable to resize image properly\n")
 	}
 
 	savePath := fmt.Sprintf("%d.%s", time.Now().UnixMicro(), extension(path))
 	err = bimg.Write(savePath, newImage)
 	if err != nil {
-		log.Fatalln("unable to write to specified path")
+		return fmt.Errorf("unable to write to specified path\n")
 	}
+
+	return nil
 }
 
-func forceResize(path string, width, height int) {
+func ForceResize(path string, width, height int) error {
 	buffer, err := bimg.Read(path)
 	if err != nil {
-		log.Fatalln("unable to find the path specified")
+		return fmt.Errorf("unable to find the path specified")
 	}
 
-	newImage, err := bimg.NewImage(buffer).Resize(width, height)
+	newImage, err := bimg.NewImage(buffer).ForceResize(width, height)
 	if err != nil {
-		log.Fatalln("unable to resize image")
+		return fmt.Errorf("unable to resize image")
 	}
 
 	savePath := fmt.Sprintf("%d.%s", time.Now().UnixMicro(), extension(path))
 	err = bimg.Write(savePath, newImage)
 	if err != nil {
-		log.Fatalln("unable to write to specified path")
+		return fmt.Errorf("unable to write to specified path")
 	}
+
+	return nil
 }
