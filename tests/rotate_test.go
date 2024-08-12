389640d74b90788165e4dc2abc318fe883b6fde6
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-12 18:12:15 +0330

    test(imagick): rotate

--- tests/rotate_test.go
+++ tests/rotate_test.go
@@ -1,7 +1,13 @@
 package test
 
-import "testing"
+import (
+	"image-processer/imagick"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestRotate(t *testing.T) {
-
+	err := imagick.Rotate("./assets/image.png", 180)
+	assert.Nil(t, err)
 }
