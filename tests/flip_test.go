b16243687c429c76bf92ce03ef6ed66c5a61c3a2
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-12 18:06:05 +0330

    test(imagick): flip

--- tests/flip_test.go
+++ tests/flip_test.go
@@ -1,7 +1,13 @@
 package test
 
-import "testing"
+import (
+	"image-processer/imagick"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestFlip(t *testing.T) {
-
+	err := imagick.Flip("./assets/image.png")
+	assert.Nil(t, err)
 }
