5144d48bbb24ead67329ff4bd8bfea527a2e397f
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-12 18:09:56 +0330

    test(imagick): resize & force-resize

--- tests/resize_test.go
+++ tests/resize_test.go
@@ -1,11 +1,18 @@
 package test
 
-import "testing"
+import (
+	"image-processer/imagick"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestResize(t *testing.T) {
-
+	err := imagick.Resize("./assets/image.png", 1000, 1000)
+	assert.Nil(t, err)
 }
 
 func TestForceResize(t *testing.T) {
-
+	err := imagick.ForceResize("./assets/image.png", 1000, 1000)
+	assert.Nil(t, err)
 }
