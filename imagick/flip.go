8748680ae2166efec0da0a166de7567e7a21a748
hosseinmirzapur <hosseinmirzapur@gmail.com>
2024-08-12 16:48:14 +0330

    feat(imagick): flip function

--- imagick/flip.go
+++ imagick/flip.go
@@ -1,1 +1,22 @@
 package imagick
+
+import "github.com/h2non/bimg"
+
+func Flip(path string) error {
+	buff, err := readFile(path)
+	if err != nil {
+		return err
+	}
+
+	newImage, err := bimg.NewImage(buff).Flip()
+	if err != nil {
+		return err
+	}
+
+	err = saveFile(newImage, path)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
